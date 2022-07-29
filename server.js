5078e475858f52e22e259224213c5fe8c093eb55
ayarem21 <ayarem21@gmail.com>
2022-07-29 21:56:16 +0300

    kek

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ require("./app/routes/question.routes")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);dfsdfsdfsdf
 });
